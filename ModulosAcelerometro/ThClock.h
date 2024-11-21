dbcfc2473a0367133effdbb87a42d8faafc08b9a
Alvarosr002 <164243089+Alvarosr002@users.noreply.github.com>
2024-11-21 16:01:24 +0100

    Añado el fichero hora.h

---
+++ ModulosAcelerometro/ThClock.h
@@ -0,0 +1,9 @@
+#ifndef __THCLOCK_H
+#define __THCLOCK_H
+
+	#include "cmsis_os2.h"                          // CMSIS RTOS header file
+	#include "stm32f4xx_hal.h"
+
+	int Init_ThClock (void);
+
+#endif /* __THCLOCK_H */
