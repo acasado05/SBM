b8503f0ef096ed14eeca3a03342ff47f31f3b2e0
Aitor Casado <145760927+acasado05@users.noreply.github.com>
2024-11-23 12:56:27 +0100

    Template del LCD pero .h

---
+++ TemplatesHiloLCD/lcd.h
@@ -0,0 +1,19 @@
+#ifndef __THLCD_H
+#define __THLCD_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "cmsis_os2.h"                          // CMSIS RTOS header file
+#include "string.h"                             // Libreria que contiene la funcion strlen
+#include "stdio.h"                                // Libreria que contiene la funcion sprintf
+#include "Driver_SPI.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+int Init_Thread (void);
+  /* Exported thread functions,  
+  Example: extern void app_main (void *arg); */
+
+#endif /* __THLCD_H */
