9af2d78c5e4f9856b05e0037a81f8f338bd8ad1f
Aitor Casado <145760927+acasado05@users.noreply.github.com>
2024-11-27 17:01:52 +0100

    Delete ModulosAcelerometro/hora.h

--- ModulosAcelerometro/hora.h
+++
@@ -1,9 +0,0 @@
-#ifndef __THCLOCK_H
-#define __THCLOCK_H
-
-	#include "cmsis_os2.h"                          // CMSIS RTOS header file
-	#include "stm32f4xx_hal.h"
-
-	int Init_ThClock (void);
-
-#endif /* __THCLOCK_H */
