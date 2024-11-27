03a1639f9372cf417fae8097532b7f1e954da80a
Aitor Casado <145760927+acasado05@users.noreply.github.com>
2024-11-27 16:56:40 +0100

    Update hora.c

--- ModulosAcelerometro/hora.c
+++ ModulosAcelerometro/hora.c
@@ -1,4 +1,4 @@
-#include "ThClock.h"
+#include "hora.h"
  
 /*----------------------------------------------------------------------------
  *                             MODULO HORA
@@ -6,84 +6,65 @@
  *---------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------
- *                                TIMER
- *--------------------------------------------------------------------------*/
-
-osTimerId_t segundero;                        // identificador del timer encargado de gestionar el tiempo transcurrido
-static uint32_t exec;
-
-extern uint8_t segundos;
-extern uint8_t minutos;
-extern uint8_t horas;
-
-static void Segundero_Callback(void const *arg){	// Callback del timer que se va a ejecutar cada segundo
-	
-	segundos++;
-	
-	if(segundos == 60){
-		
-		segundos = 0;
-		minutos++;
-	}	
-	
-	if(minutos == 60){
-		
-		minutos = 0;
-		horas++;
-	}
-	
-	if(horas == 24){
-		
-		horas = 0;
-	}
-	
-}
-
-int Init_Segundero(void){
-	
-	osStatus_t status;
-	
-	// Creacion de un timer
-	exec = 2U;
-	segundero = osTimerNew((osTimerFunc_t)&Segundero_Callback, osTimerPeriodic, &exec, NULL);
-	if(segundero != NULL){	// Si el timer se ha creado correctamente
-		// Se arranca el timer con un periodo de 1 segundo
-		// status = osTimerStart(segundero, 1000);
-		if(status != osOK){
-			
-			return -1;
-		}
-		
-	}
-	
-	return NULL;
-}
-
-/*--------------------------------------------------------------------------
  *                                HILO
  *--------------------------------------------------------------------------*/
+osTimerId_t segundero;                        // identificador del timer encargado de gestionar el tiempo transcurrido
+osStatus_t status_segundero;
+static uint32_t exec;
  
-osThreadId_t tid_ThClock;                        // identificador del hilo encargado de gestionar la hora
+osThreadId_t tid_hora;                        // identificador del hilo encargado de gestionar la hora
  
 void ThClock (void *argument);                   // funcion principal del hilo encargado  de gestionar la hora
+
+uint8_t segundos;
+uint8_t minutos;
+uint8_t horas;
+
+void Segundero_Callback(void const *arg);
  
-int Init_ThClock (void) {
+int Init_Hora (void) {
  
-  tid_ThClock = osThreadNew(ThClock, NULL, NULL);
-  if (tid_ThClock == NULL) {
+  tid_hora = osThreadNew(ThClock, NULL, NULL);
+  if (tid_hora == NULL) {
     return(-1);
   }
-	
-	Init_Segundero();
+  
+  segundero = osTimerNew((osTimerFunc_t)&Segundero_Callback, osTimerPeriodic, &exec, NULL);
+  if(segundero != NULL){	// Si el timer se ha creado correctamente
+  // Se arranca el timer con un periodo de 1 segundo
+  // status = osTimerStart(segundero, 1000);
+  if(status_segundero != osOK){
+    return -1;
+  }
+}
   return(0);
 }
  
 void ThClock (void *argument) {
-	
-	osTimerStart(segundero, 1000);
-	
+  
+  osTimerStart(segundero, 1000);
+  
   while (1) {
-    ; // Insert thread code here...
+    // Insert thread code here...
     osThreadYield();                            // suspend thread
+  }
+}
+
+void Segundero_Callback(void const *arg){ // Callback del timer que se va a ejecutar cada segundo
+  
+  segundos++;
+  
+  if(segundos == 60){
+    segundos = 0;
+    minutos++;
+  }
+  
+  if(minutos == 60){
+    minutos = 0;
+    horas++;
+  }
+  
+  if(horas == 24){
+    horas = 0;
   }
 }
