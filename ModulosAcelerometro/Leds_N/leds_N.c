99d53d04975432af202db9e27edaab098570b91c
Aitor Casado <145760927+acasado05@users.noreply.github.com>
2024-12-08 20:59:14 +0100

    Add files via upload

--- ModulosAcelerometro/Leds_N/leds_N.c
+++ ModulosAcelerometro/Leds_N/leds_N.c
@@ -6,59 +6,59 @@
 
 float Ax_r, Ay_r, Az_r = 1.0;
 
-osThreadId_t tid_ThLeds;                        // thread id
+osThreadId_t tid_leds;                        // thread id
  
 void ThLeds (void *argument);                   // thread function
  
 void Init_LEDs(void);
  
-int Init_ThLEDs (void) {
+int Init_leds_N (void) {
  
-	// HILO
-  tid_ThLeds = osThreadNew(ThLeds, NULL, NULL);
-  if (tid_ThLeds == NULL) {
+  // HILO
+  tid_leds = osThreadNew(ThLeds, NULL, NULL);
+  if (tid_leds == NULL) {
     return(-1);
   }
  
-	Init_LEDs();
-	
+  Init_LEDs();
+  
   return(0);
 }
  
 void ThLeds (void *argument) {
 
-	uint32_t flag_recibido;
-	
+  uint32_t flag_recibido;
+  
   while (1) {
     // Insert thread code here...
     
-		flag_recibido = osThreadFlagsWait(0X3F, osFlagsWaitAny, osWaitForever);
-		
-		if(LED1_ON & flag_recibido){
-				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-		}
-		
-		if (LED1_OFF & flag_recibido){
-				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-		}
-		
-		if (LED2_ON & flag_recibido){
-				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
-		}
-		
-		if (LED2_OFF & flag_recibido){
-				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-		}
-		
-		if (LED3_ON & flag_recibido){
-				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
-		}
-		
-		if (LED3_OFF & flag_recibido){
-				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
-		}
-		
-		//osThreadYield();                            // suspend thread
+    flag_recibido = osThreadFlagsWait(0X3F, osFlagsWaitAny, osWaitForever);
+    
+    if(LD1_ON & flag_recibido){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+    }
+    
+    if (LD1_OFF & flag_recibido){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+    }
+    
+    if (LD2_ON & flag_recibido){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+    }
+    
+    if (LD2_OFF & flag_recibido){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+    }
+    
+    if (LD3_ON & flag_recibido){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+    }
+    
+    if (LD3_OFF & flag_recibido){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+    }
+    
+    //osThreadYield();                            // suspend thread
   }
 }
 
@@ -67,17 +67,17 @@ void ThLeds (void *argument) {
  *--------------------------------------------------------------------------*/
 
 void Init_LEDs(void){
-	
-	GPIO_InitTypeDef GPIO_InitStruct;
-	
-	__HAL_RCC_GPIOB_CLK_ENABLE();	// Habilitar el reloj asociado al puerto de los LEDs. En este caso el reloj del puerto B
-	
-	// Configuracion de los LEDs VERDE|AZUL|ROJO  = PIN 0|7|14
-	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_7 | GPIO_PIN_14;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-	
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-	
+  
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  __HAL_RCC_GPIOB_CLK_ENABLE();	// Habilitar el reloj asociado al puerto de los LEDs. En este caso el reloj del puerto B
+  
+  // Configuracion de los LEDs VERDE|AZUL|ROJO  = PIN 0|7|14
+  GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_7 | GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  
 };
--- ModulosAcelerometro/Leds_N/leds_N.h
+++ ModulosAcelerometro/Leds_N/leds_N.h
@@ -1,19 +1,23 @@
 #ifndef __LEDS_N_H
 #define __LEDS_N_H
 
+/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 #include "cmsis_os2.h"                          // CMSIS RTOS header file
 
-#define LED1_ON  0X01U
-#define LED1_OFF 0X02U
-#define LED2_ON  0X04U
-#define LED2_OFF 0X08U
-#define LED3_ON  0X10U
-#define LED3_OFF 0X20U
-
-	extern osThreadId_t tid_ThLeds;                        // thread id
-	
-	int Init_ThLEDs (void);
-	int Init_Thread (void);
+/* Exported types ------------------------------------------------------------*/
+extern osThreadId_t tid_leds;                        // thread id
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+#define LD1_ON  0x01U
+#define LD1_OFF 0x02U
+#define LD2_ON  0x04U
+#define LD2_OFF 0x08U
+#define LD3_ON  0x10U
+#define LD3_OFF 0x20U
+/* Exported functions ------------------------------------------------------- */
+extern int Init_leds_N (void);
+  /* Exported thread functions,  
+  Example: extern void app_main (void *arg); */
 
 #endif /* __LEDS_N_H */
